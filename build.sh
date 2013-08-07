8b7ceef3e17487124408669363c70c5821064671
Sean Robertson <sprobertson@gmail.com>
2013-08-07 16:44:18 -0700

    proper build script

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 # Ugly hack to prepend shebang line
 coffee -o bin -c src
-cp bin/devolve.js bin/devolve.js.tmp
-echo "#!/usr/bin/env node" > bin/devolve.js.tmp
-cat bin/devolve.js >> bin/devolve.js.tmp
-mv bin/devolve.js.tmp bin/devolve.js
+cp bin/ignitionx.js bin/ignitionx.js.tmp
+echo "#!/usr/bin/env node" > bin/ignitionx.js.tmp
+cat bin/ignitionx.js >> bin/ignitionx.js.tmp
+mv bin/ignitionx.js.tmp bin/ignitionx.js
